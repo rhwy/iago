2b9257293594a4410509564cca1e34bc6e5a2742
Rui Carvalho <github@rui.fr>
2015-02-16 02:26:13 +0100

    [experiment] add runner as a dependent tool/lib and add command

---
+++ src/runner/host.cs
@@ -0,0 +1,11 @@
+
+namespace Iago.Runner {
+  using static System.Console;
+
+  public class Program {
+    public static void Main(params string[] args)
+    {
+      WriteLine("--- IAGO Runner ---");
+    }
+  }
+}
