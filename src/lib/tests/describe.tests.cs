a10c6096cd6c4e2cb54d6a8df18baa2f8b11c3d1
Rui Carvalho <github@rui.fr>
2015-02-17 02:12:29 +0100

    [clean] remove temporarly actual xunit tests

--- src/lib/tests/describe.tests.cs
+++ src/lib/tests/describe.tests.cs
@@ -1,35 +1,17 @@
-using Xunit;
-using NFluent;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-
-using static NDescribe.Specs;
-using static System.Console;
-
-namespace NDescribe.Tests
+namespace Iago.Tests
 {
-  public class CoffeeMachineSpecs
-  {
-    Specify that = () =>
-        "CoffeeMachine Sould automate the production of coffee";
+  using Xunit;
+  using NFluent;
+  using System;
+  using System.Collections.Generic;
+  using System.Reflection;
 
-    public void Run()
-    {
-        When("coffee machine is started", ()=> {
-          var machine = new CoffeeMachine();
-          machine.Start();
-
-          Then("screen must be equal to 'Welcome'", ()=>{
-            Check.That(machine.Screen).IsEqualTo("Welcome");
-          });
-        });
-    }
-  }
+  using static Iago.Specs;
+  using static System.Console;
 
   public class CoffeeMachineSpecsTests
   {
-    [Fact]
+  /*  [Fact]
     public void run_the_specs()
     {
       var specs = new CoffeeMachineSpecs();
@@ -38,10 +20,7 @@ namespace NDescribe.Tests
       var specMessage = myFieldInfo1.GetValue(specs) as Specify;
       WriteLine("[spec] " + specMessage());
       specs.Run();
-    }
+    }*/
   }
-  public class CoffeeMachine{
-    public void Start(){}
-    public string Screen {get;} = "Welcome";
-  }
+
 }
