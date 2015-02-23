95723484f7e2f30923a6a651885454b4313da58f
Rui Carvalho <github@rui.fr>
2015-02-23 17:38:03 +0100

    [feat] run and load sample test lib

--- src/runner/Bootstrap/applicationHost.cs
+++ src/runner/Bootstrap/applicationHost.cs
@@ -1,5 +1,11 @@
 namespace Iago.Runner
 {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Reflection;
+  using static System.Console;
+
   public class ApplicationHost
   {
     public HostedConfiguration Configuration {get;}
@@ -11,7 +17,24 @@ namespace Iago.Runner
 
     public void Run()
     {
-      var domainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+      //var domainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+      var domainAssemblies = new []{ Assembly.Load(Configuration.HostedApplicationName)};
+      var specTypes = new List<Type>();
+      WriteLine("scanning ...");
+      foreach(var asm in domainAssemblies)
+      {
+        WriteLine("--> " + asm.FullName);
+        asm.ExportedTypes
+          .Where(type => type.Name.ToLower().EndsWith("specs"))
+          .ToList()
+          .ForEach(type=> specTypes.Add(type));
+      }
+
+      WriteLine("specs found:");
+      foreach(var spec in specTypes)
+      {
+        WriteLine("\t - " + spec.Name);
+      }
     }
   }
 
