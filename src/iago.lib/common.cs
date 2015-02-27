d8e8abc56e28ec32f3db599697f2733f097e1ce0
Rui Carvalho <github@rui.fr>
2015-02-27 01:21:48 +0100

    [feat] added a simple railway mechanism in common

--- src/iago.lib/common.cs
+++ src/iago.lib/common.cs
@@ -5,8 +5,60 @@ namespace Iago {
   {
     T Value {get;}
     bool AsExpected {get;}
-    Result<U> Then<U>(Func<T,U> success, Func<Exception> error);
+    Exception Error {get;}
+
   }
 
+  public class Success<T> : Result<T>
+  {
+      public T Value {get;}
+      public bool AsExpected {get;} = true;
+      public Exception Error {get;} = null;
+      public Success(T value)
+      {
+        Value = value;
+      }
+  }
+  public class Failure<T> : Result<T>
+  {
+      public T Value {get;}
+      public bool AsExpected {get;} = false;
+      public Exception Error {get;} = null;
+      public Failure(Exception error)
+      {
+        Error = error;
+      }
+  }
+  public static class ResultExtensions
+  {
+    public static Result<U> Then<T,U>(
+      this Result<T> result, Func<T,U> success, Func<Exception,Exception> error)
+      {
+        if(result.AsExpected)
+        {
+          return new Success<U>(success(result.Value));
+        }
+        return new Failure<U>(error(result.Error));
+      }
+
+      public static Result<U> Then<T,U>(
+        this Result<T> result, Func<T,U> success)
+        {
+          if(result.AsExpected)
+          {
+            return new Success<U>(success(result.Value));
+          }
+          return new Failure<U>(result.Error);
+        }
+  }
   public struct Unit {}
+
+  public class Option<T>
+  {
+    public T Value {get;}
+    public Option(T value)
+    {
+      Value = value;
+    }
+  }
 }
