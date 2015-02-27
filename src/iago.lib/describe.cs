839ec7d1c51a5c3262330d68eced31e82bbd7436
Rui Carvalho <github@rui.fr>
2015-02-28 00:54:02 +0100

    [clean] added logger ref to lib

--- src/iago.lib/describe.cs
+++ src/iago.lib/describe.cs
@@ -1,12 +1,19 @@
-using static System.Console;
-
 namespace Iago
 {
+  using System;
+  using Microsoft.Framework.Logging;
+
+  using static System.Console;
+
   public delegate string Specify();
   public delegate string Description();
   public delegate void TestAction();
 
   public static class Specs {
+    private static ILogger logger;
+    public static Func<ILogger> SetLogger = ()=> {
+        throw new NotImplementedException(); return null;};
+
     public static void When(string definition, TestAction act) {
       WriteLine("\t[when] "+definition);
       act();
@@ -18,6 +25,7 @@ namespace Iago
 
     public static void And(string definition, TestAction assert){
       WriteLine("\t[.And] "+definition);
+      assert();
     }
   }
 }
