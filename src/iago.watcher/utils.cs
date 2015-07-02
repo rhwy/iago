d349ed7030fb8a6531f49624f90daa2b0b296cb4
Rui Carvalho <code@rui.fr>
2015-07-02 03:02:40 +0200

    [rc-feat] imported some utilities

---
+++ src/iago.watcher/utils.cs
@@ -0,0 +1,84 @@
+namespace Utilities
+{
+	using System;
+	using System.Collections.Generic;
+	
+	public static class ApplicationTime
+	{
+		public static Func<DateTime> NowDefinition {private get;set;} 
+			= ()=>DateTime.Now;
+		public static DateTime Now { get { return NowDefinition();}}
+	}
+	
+	
+	public class MatchNotFoundException : Exception 
+	{ 
+	    public MatchNotFoundException(string message) : base(message) { } 
+	}
+	
+	public class PatternMatch<T, TResult> 
+	{ 
+	    private readonly T value; 
+	    private readonly List<Tuple<Predicate<T>, Func<T, TResult>>> cases  
+	        = new List<Tuple<Predicate<T>, Func<T, TResult>>>(); 
+	    private Func<T, TResult> elseFunc;
+	
+	    internal PatternMatch(T value) 
+	    { 
+	        this.value = value; 
+	    }
+	
+	    public PatternMatch<T, TResult> With(Predicate<T> condition, Func<T, TResult> result) 
+	    { 
+	        cases.Add(Tuple.Create(condition, result)); 
+	        return this; 
+	    }
+	
+	    public PatternMatch<T, TResult> Else(Func<T, TResult> result) 
+	    { 
+	        if (elseFunc != null) 
+	            throw new InvalidOperationException("Cannot have multiple else cases");
+	
+	        elseFunc = result; 
+	        return this; 
+	    }
+	
+	    public TResult Do() 
+	    { 
+	        if (elseFunc != null) 
+	            cases.Add( 
+	                Tuple.Create<Predicate<T>, Func<T, TResult>>(x => true, elseFunc)); 
+	        foreach (var item in cases) 
+	            if (item.Item1(value)) 
+	                return item.Item2(value);
+	
+	        throw new MatchNotFoundException("Incomplete pattern match"); 
+	    } 
+	}
+	
+	
+	public class PatternMatchContext<T> 
+	{ 
+	    private readonly T value;
+	    internal PatternMatchContext(T value) 
+	    { 
+	        this.value = value; 
+	    }
+	
+	    public PatternMatch<T, TResult> With<TResult>( 
+	        Predicate<T> condition,  
+	        Func<T, TResult> result) 
+	    { 
+	        var match = new PatternMatch<T, TResult>(value); 
+	        return match.With(condition, result); 
+	    } 
+	}
+	
+	public static class PatternMatchExtensions 
+	{ 
+	    public static PatternMatchContext<T> Match<T>(this T value) 
+	    { 
+	        return new PatternMatchContext<T>(value); 
+	    } 
+	}
+}
