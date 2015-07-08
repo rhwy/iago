6e1ea2ca50c9fee0bd042dee001f07b072d8b7fd
Rui Carvalho <code@rui.fr>
2015-07-08 17:02:50 +0200

    [rc-clean] enhanced process lunch to try to avoir bad printing

--- src/iago.watcher/app.cs
+++ src/iago.watcher/app.cs
@@ -8,38 +8,68 @@ namespace Iago.FolderMonitor
 	using Utilities;
 	
 	using FSWatcher;
-	
+	using System.Diagnostics;
 	
 	
 	public class Program
 	{
 		IApplicationEnvironment environment;
+		static DateTime start = ApplicationTime.Now;
+		static Func<string> stamp = () => new DateTime((ApplicationTime.Now-start).Ticks).ToString("ss:ffffff");
+		
+		Action<string> log = (m) => {
+			WriteLine($"[{stamp()}] {m}"); 
+		};
 		
 		public Program(IApplicationEnvironment appEnv)
 	    {
 	 
 	      environment = appEnv;
-		  var start = ApplicationTime.Now;
+		  
 	      var asm = System.Reflection.Assembly.GetExecutingAssembly();
 	      var appVersion = asm.GetName().Version.ToString(); 
-	      WriteLine($"==== DNX Watcher, v{appVersion} ===");
-		  Func<string> stamp = () => new DateTime((ApplicationTime.Now-start).Ticks).ToString("ss:ffffff");
-		  WriteLine($"[{stamp()}] watch folder : {appEnv.ApplicationBasePath}");
-		  WriteLine($"[{stamp()}] started watch"); 
+	      
+		  WriteLine($"==== DNX Watcher, v{appVersion} ===");
+		  
+		  log($"watch folder : {appEnv.ApplicationBasePath}");
+		  log("started watch");
 	    }
+		
+		
 		public void Main(string[] args)
 		{
 			var configurationBuilder = new ConfigurationBuilder(
 					environment.ApplicationBasePath)
-          		.AddJsonFile("watcher.json")
+          		.AddJsonFile("watcher.json",true)
 				.AddCommandLine(args)
 				;
 				
         	IConfiguration configuration = configurationBuilder.Build();
-         	
+         	 
 			 Action<string> onChange = 
 			 	(fsElement) => {
-					 WriteLine(fsElement);
+					 var dnx = configuration.Get("dnx");
+					 var started = ApplicationTime.Now;
+					 
+					 if(!string.IsNullOrEmpty(dnx))
+					 {
+						 WriteLine($"ready to {dnx}");
+						 
+						 var info = new ProcessStartInfo(
+							 filename:"dnx",
+							 arguments:environment.ApplicationBasePath + " " +dnx
+						 );
+						 lock (Console.Out)
+						 {
+							 using(Process process = Process.Start(info))
+							 {
+								 process.EnableRaisingEvents = true;
+								 process.WaitForExit();
+				            	 var stoped = (ApplicationTime.Now - started).TotalMilliseconds;
+					             Console.WriteLine($"Done in {stoped}ms");
+				            }
+						 }
+					 }
 				 };
 			var watcher = 
 				new Watcher(
@@ -52,18 +82,28 @@ namespace Iago.FolderMonitor
 					
 			watcher.ErrorNotifier((path, ex) => { System.Console.WriteLine("{0}\n{1}", path, ex); });
     
-			WriteLine("key:"+configuration.Get("pollFrequency"));
 			int pollFreq;
 			if(int.TryParse(configuration.Get("pollFrequency"), out pollFreq))
 			{
-				WriteLine("freq:{0}",pollFreq);
 				watcher.Settings.SetPollFrequencyTo(pollFreq); 
-				WriteLine("freq:{0}",watcher.Settings.PollFrequency);
 			}
 			
 			watcher.Watch();
-			var command = System.Console.ReadLine();
-            
+			onChange("start");
+		    
+			Func<ConsoleKeyInfo,bool> checkKey = 
+			(key)=>{
+				return key.Key != ConsoleKey.Escape;	
+			};
+			
+            ConsoleKeyInfo cki;
+		    Console.TreatControlCAsInput = true;
+		
+		    do {
+		         cki = Console.ReadKey(false);
+		   } while (checkKey(cki));
+	  
+	  
 			watcher.StopWatching();
 		}
 	}
